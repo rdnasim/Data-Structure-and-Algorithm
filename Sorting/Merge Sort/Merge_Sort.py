be3ed0c6e785cb5a9acc79a0911d2a92a4cef728
rdnasim <rdislamnasim78@gmail.com>
2017-08-13 00:54:02 +0600

    Update Sorting Algorithm

---
+++ Sorting/Merge Sort/Merge_Sort.py
@@ -0,0 +1,23 @@
+def merge(left, right):
+    result = []
+    i, j = 0, 0
+    while i< len(left) and j < len(right):
+        if (left[i] <= right[j]):
+            result.append(left[i])
+            i += 1
+        else:
+            result.append(right[j])
+            j += 1
+    result += left[i:]
+    result += right[j:]
+    return result
+
+def merge_sort(list):
+    if(len(list) <= 1):
+        return list
+    mid = int(len(list)/2)
+    left = merge_sort(list[:mid])
+    right = merge_sort(list[mid:])
+    return merge(left, right)
+arr = [8,2,6,4]
+print (merge_sort(arr))
