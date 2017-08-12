2ca4eb986a7d98e80c89324681648e403e419599
rdnasim <rdislamnasim78@gmail.com>
2017-08-12 23:27:43 +0600

    Update Bubble Sorting Algorithm

--- Sorting/Bubble Sort/Bubble_Sort.py
+++ Sorting/Bubble Sort/Bubble_Sort.py
@@ -1,9 +1,10 @@
 def bubble_sort(list):
-    for i in range(0, len(list)-1):
-        for j in range(0, len(list)-1-i):
-            if list[j] > list[j+1]:
-                list[j], list[j+1] = list[j+1], list[j]
+    for i in range(0, len(list) - 1):
+        for j in range(0, len(list) - 1 - i):
+            if list[j] > list[j + 1]:
+                list[j], list[j + 1] = list[j + 1], list[j]
     return list
 
-list = [3,2,4,1]
+
+list = [5,1,4,2,8]
 print(bubble_sort(list))
